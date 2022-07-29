8d9bf484ec0933c992b7a0ac7856466304a5e1f3
lemon <992273994@qq.com>
2022-07-29 22:01:38 +0800

    add

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 const express = require('express')
 const app = express()
 const { faker } = require('@faker-js/faker')
+const swaggerJsdoc = require("swagger-jsdoc"),
+    swaggerUi = require("swagger-ui-express");
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "LogRocket Express API with Swagger",
+            version: "0.1.0",
+            description:
+                "This is a simple CRUD API application made with Express and documented with Swagger",
+            license: {
+                name: "MIT",
+                url: "https://spdx.org/licenses/MIT.html",
+            },
+
+        },
+        servers: [
+            {
+                url: "http://localhost",
+            },
+        ],
+    },
+    apis: ["./routes/books.js"],
+};
+const specs = swaggerJsdoc(options);
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs, { explorer: true })
+);
 
 const getItem = () => {
     const post = {
---
+++ routes/books.js
@@ -0,0 +1,21 @@
+/**
+* @swagger
+*    components:
+*      schemas:
+*        post:
+*          type: object
+*          properties:
+*            title:
+*              type: string 
+*              description: 标题
+*            avatar:
+*              type: string
+*              description: 头像.
+*            image:
+*              type: string
+*              description: 图片
+*            vote:
+*              type: number
+*              description: 点赞
+
+*/
