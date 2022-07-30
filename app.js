1e174c3fb56022ae9a5738e0a9988cf771065f0f
lemon <992273994@qq.com>
2022-07-30 16:20:07 +0800

    22

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 var bodyParser = require('body-parser')
 const { faker } = require('@faker-js/faker')
 const swaggerJsdoc = require("swagger-jsdoc"),
     swaggerUi = require("swagger-ui-express");
+const { v4: uuidv4 } = require('uuid');
 const options = {
     definition: {
         openapi: "3.0.0",
@@ -27,7 +29,12 @@ const options = {
     apis: ["./routes/books.js"],
 };
 const specs = swaggerJsdoc(options);
+const cache = {
+
+}
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
 app.use(
     "/api-docs",
     swaggerUi.serve,
@@ -60,5 +67,35 @@ app.get('/list', (req, res) => {
         data: getList(req?.query?.pageSize || 8)
     })
 })
+
+app.post('/mind', (req, res) => {
+    console.log(req.body.data);
+    const id = uuidv4()
+    cache[id] = {
+        data: req.body.data,
+        time: Math.ceil(new Date() / 1000)
+    }
+    console.log(cache);
+    res.json({
+        id: id,
+        cache: cache
+    })
+})
+app.get('/mind', (req, res) => {
+    res.json({
+        data: cache[req.query.id],
+
+    })
+})
+
+setInterval(() => {
+    for (let prop in cache) {
+        const time = cache[prop]['time']
+        const delta = Math.ceil(new Date() / 1000) - time
+        if (delta > 60 * 60 * 24 * 30) {
+            cache[prop] = null
+        }
+    }
+}, 1000 * 60 * 60 * 24);
 
 app.listen('80')
